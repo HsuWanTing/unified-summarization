Fix bugs: multiply padding mask when calculating sentence features and article features. Print avg precision, avg recall and avg accuracy on tensorboard.

sentence-selector/batcher.py
@@ -52,7 +52,7 @@ class Example(object):
 
     self.art_ids = []
     self.sent_lens = []
-    for sent in art_words:
+    for sent in article_sentences:
       sent = sent.split()
       if len(sent) > hps.max_sent_len:
         sent = sent[:hps.max_sent_len]
@@ -85,7 +85,7 @@ class Example(object):
     # Store the original strings
     self.original_article_sents = article_sentences
     self.original_extract_sent_ids = extract_sent_ids
-    self.original_abstract = abstract
+    #self.original_abstract = abstract
     self.original_abstract_sents = abstract_sentences
 
 
@@ -180,8 +180,8 @@ class Batch(object):
           Same as self.enc_batch, but in-article OOVs are represented by their temporary article OOV number.
     """
     # Determine the maximum length of the encoder input sequence in this batch
-    #max_enc_seq_len = max([ex.enc_len for ex in example_list])
-    max_enc_seq_len = hps.max_art_len
+    #self.max_art_len = max([ex.art_len for ex in example_list])
+    #max_enc_seq_len = hps.max_art_len
 
     # Pad the encoder input sequences up to the length of the longest sequence
     for ex in example_list:
@@ -191,17 +191,20 @@ class Batch(object):
     # Note: our enc_batch can have different length (second dimension) for each batch because we use dynamic_rnn for the encoder.
     self.art_batch = np.zeros((hps.batch_size, hps.max_art_len, hps.max_sent_len), dtype=np.int32)
     self.art_lens = np.zeros((hps.batch_size), dtype=np.int32)
-    self.sent_lens = []
-    #self.enc_padding_mask = np.zeros((hps.batch_size, max_enc_seq_len), dtype=np.float32)
+    self.sent_lens = np.zeros((hps.batch_size, hps.max_art_len), dtype=np.int32)
+    self.art_padding_mask = np.zeros((hps.batch_size, hps.max_art_len), dtype=np.float32)
+    self.sent_padding_mask = np.zeros((hps.batch_size, hps.max_art_len, hps.max_sent_len), dtype=np.float32)
 
     # Fill in the numpy arrays
     for i, ex in enumerate(example_list):
       self.art_batch[i, :, :] = np.array(ex.art_ids)
       self.art_lens[i] = ex.art_len
-      self.sent_lens += ex.sent_lens
-      #for j in xrange(ex.enc_len):
-      #  self.enc_padding_mask[i][j] = 1
-    self.sent_lens = np.array(self.sent_lens)
+      self.sent_lens[i, :] = np.array(ex.sent_lens)
+      for j in xrange(ex.art_len):
+        self.art_padding_mask[i][j] = 1.0
+      for j in xrange(hps.max_art_len):
+        for k in xrange(ex.sent_lens[j]):
+          self.sent_padding_mask[i][j][k] = 1.0
 
     '''
     # For pointer-generator mode, need to store some extra info
@@ -220,7 +223,8 @@ class Batch(object):
     self.target_batch = np.zeros((hps.batch_size, hps.max_art_len), dtype=np.float32)
     for i, ex in enumerate(example_list):
        for idx in ex.extract_sent_ids:
-         self.target_batch[i][idx] = 1.0
+         if idx < hps.max_art_len:
+           self.target_batch[i][idx] = 1.0
 
   def init_decoder_seq(self, example_list, hps):
     """Initializes the following:
@@ -252,7 +256,7 @@ class Batch(object):
     """Store the original article and abstract strings in the Batch object"""
     self.original_articles_sents = [ex.original_article_sents for ex in example_list] # list of lists
     self.original_extract_sent_ids = [ex.original_extract_sent_ids for ex in example_list]
-    self.original_abstracts = [ex.original_abstract for ex in example_list] # list of lists
+    #self.original_abstracts = [ex.original_abstract for ex in example_list] # list of lists
     self.original_abstracts_sents = [ex.original_abstract_sents for ex in example_list] # list of list of lists
 
 
@@ -363,7 +367,7 @@ class Batcher(object):
         inputs = []
         for _ in xrange(self._hps.batch_size * self._bucketing_cache_size):
           inputs.append(self._example_queue.get())
-        inputs = sorted(inputs, key=lambda inp: inp.enc_len) # sort by length of encoder sequence
+        #inputs = sorted(inputs, key=lambda inp: inp.enc_len) # sort by length of encoder sequence
 
         # Group the sorted Examples into batches, optionally shuffle the batches, and place in the batch queue.
         batches = []

sentence-selector/model.py
@@ -39,9 +39,10 @@ class SentenceSelector(object):
     hps = self._hps
     self._art_batch = tf.placeholder(tf.int32, [hps.batch_size, hps.max_art_len, hps.max_sent_len], name='art_batch')
     self._art_lens = tf.placeholder(tf.int32, [hps.batch_size], name='art_lens')
-    self._sent_lens = tf.placeholder(tf.int32, [hps.batch_size*hps.max_art_len], name='sent_lens')
-    #self._art_padding_mask = tf.placeholder(tf.float32, [hps.batch_size, None, None], name='enc_padding_mask')
-    self._target_batch = tf.placeholder(tf.int32, [hps.batch_size, hps.max_art_len], name='target_batch')
+    self._sent_lens = tf.placeholder(tf.int32, [hps.batch_size, hps.max_art_len], name='sent_lens')
+    self._art_padding_mask = tf.placeholder(tf.float32, [hps.batch_size, hps.max_art_len], name='art_padding_mask')
+    self._sent_padding_mask = tf.placeholder(tf.float32, [hps.batch_size, hps.max_art_len, hps.max_sent_len], name='sent_padding_mask')
+    self._target_batch = tf.placeholder(tf.float32, [hps.batch_size, hps.max_art_len], name='target_batch')
 
 
   def _make_feed_dict(self, batch, just_enc=False):
@@ -56,8 +57,9 @@ class SentenceSelector(object):
     feed_dict = {}
     feed_dict[self._art_batch] = batch.art_batch # (batch_size, max_art_len, max_sent_len)
     feed_dict[self._art_lens] = batch.art_lens   # (batch_size, )
-    feed_dict[self._sent_lens] = batch.sent_lens # (batch_size*max_art_len, ), article1_sents + article2_sents + ...
-    #feed_dict[self._art_padding_mask] = batch.art_padding_mask # (batch_size, max_art_len, max_sent_len)
+    feed_dict[self._sent_lens] = batch.sent_lens # (batch_size, max_art_len)
+    feed_dict[self._art_padding_mask] = batch.art_padding_mask # (batch_size, max_art_len)
+    feed_dict[self._sent_padding_mask] = batch.sent_padding_mask # (batch_size, max_art_lens, max_sent_len)
     feed_dict[self._target_batch] = batch.target_batch # (batch_size, max_art_len)
     return feed_dict
 
@@ -74,19 +76,27 @@ class SentenceSelector(object):
         A tensor of shape [batch_size, <=max_enc_steps, 2*hidden_dim]. It's 2*hidden_dim because it's the concatenation of the forwards and backwards states.
     """
     with tf.variable_scope(name):
-      cell_fw = tf.contrib.rnn.LSTMCell(self._hps.hidden_dim, initializer=self.rand_unif_init, state_is_tuple=True)
-      cell_bw = tf.contrib.rnn.LSTMCell(self._hps.hidden_dim, initializer=self.rand_unif_init, state_is_tuple=True)
+      if self._hps.rnn_type == 'LSTM':
+        cell_fw = tf.contrib.rnn.LSTMCell(self._hps.hidden_dim, initializer=self.rand_unif_init, state_is_tuple=True)
+        cell_bw = tf.contrib.rnn.LSTMCell(self._hps.hidden_dim, initializer=self.rand_unif_init, state_is_tuple=True)
+      elif self._hps.rnn_type == 'GRU':
+        cell_fw = tf.contrib.rnn.GRUCell(self._hps.hidden_dim)
+        cell_bw = tf.contrib.rnn.GRUCell(self._hps.hidden_dim)
       (encoder_outputs, (_, _)) = tf.nn.bidirectional_dynamic_rnn(cell_fw, cell_bw, encoder_inputs, dtype=tf.float32, sequence_length=seq_len, swap_memory=True)
       encoder_outputs = tf.concat(axis=2, values=encoder_outputs) # concatenate the forwards and backwards states
     return encoder_outputs
 
 
   def _add_classifier(self, sent_feats, art_feats):
-    """a logistic layer makes a binary decision as to whether the sentence belongs to the summary"""
+    """a logistic layer makes a binary decision as to whether the sentence belongs to the summary
+       sent_feats: sentence representations, shape (batch_size, max_art_len of this batch, hidden_dim*2)
+       art_feats: article representations, shape (batch_size, hidden_dim*2)"""
     hidden_dim = self._hps.hidden_dim
     batch_size = self._hps.batch_size
+    #max_art_len = sent_feats.shape[1]
 
     with tf.variable_scope('classifier'):
+      """
       w_content = tf.get_variable('w_content', [hidden_dim * 2, 1], dtype=tf.float32, initializer=self.trunc_norm_init)
       w_salience = tf.get_variable('w_salience', [hidden_dim * 2, hidden_dim * 2], dtype=tf.float32, initializer=self.trunc_norm_init)
       w_novelty = tf.get_variable('w_novelty', [hidden_dim * 2, hidden_dim * 2], dtype=tf.float32, initializer=self.trunc_norm_init)
@@ -94,20 +104,30 @@ class SentenceSelector(object):
 
       # s is the dynamic representation of the summary at the j-th sentence
       s = tf.zeros([batch_size, hidden_dim * 2])
+      """
+      w_content = tf.get_variable('w_content', [hidden_dim, 1], dtype=tf.float32, initializer=self.trunc_norm_init)
+      w_salience = tf.get_variable('w_salience', [hidden_dim, hidden_dim], dtype=tf.float32, initializer=self.trunc_norm_init)
+      w_novelty = tf.get_variable('w_novelty', [hidden_dim, hidden_dim], dtype=tf.float32, initializer=self.trunc_norm_init)
+      bias = tf.get_variable('bias', [1], dtype=tf.float32, initializer=tf.zeros_initializer())
+
+      # s is the dynamic representation of the summary at the j-th sentence
+      s = tf.zeros([batch_size, hidden_dim])
 
       logits = [] # logits before the sigmoid layer
+      probs = []
 
       for i in range(self._hps.max_art_len):
-        content_feats = tf.matmul(sent_feats[:, i, :], w_content) # (batch_size, )
-        salience_feats = tf.reduce_sum(tf.matmul(sent_feats[:, i, :], w_salience) * art_feats, 1) # (batch_size, )
-        novelty_feats = tf.reduce_sum(tf.matmul(sent_feats[:, i, :], w_novelty) * tf.tanh(s), 1) # (batch_size, )
-        logit = content_feats + salience_feats - novelty_feats + bias
+        content_feats = tf.matmul(sent_feats[:, i, :], w_content) # (batch_size, 1)
+        salience_feats = tf.reduce_sum(tf.matmul(sent_feats[:, i, :], w_salience) * art_feats, 1, keep_dims=True) # (batch_size, 1)
+        novelty_feats = tf.reduce_sum(tf.matmul(sent_feats[:, i, :], w_novelty) * tf.tanh(s), 1, keep_dims=True) # (batch_size, 1)
+        logit = content_feats + salience_feats - novelty_feats + bias # (batch_size, 1)
         logits.append(logit)
 
-        p = tf.sigmoid(logit) # (batch_size, )
+        p = tf.sigmoid(logit) # (batch_size, 1)
+        probs.append(p)
         s += tf.multiply(sent_feats[:, i, :], p)
 
-      return tf.stack(logits, 1) # (batch_size, max_art_len)
+      return tf.concat(logits, 1), tf.concat(probs, 1)  # (batch_size, max_art_len)
     
 
   def _add_emb_vis(self, embedding_var):
@@ -148,29 +168,41 @@ class SentenceSelector(object):
       ########################################
       # Add word-level encoder to encode each sentence.
       sent_enc_inputs = tf.reshape(emb_batch, [-1, hps.max_sent_len, hps.emb_dim]) # (batch_size*max_art_len, max_sent_len, emb_dim)
-      sent_enc_outputs = self._add_encoder(sent_enc_inputs, self._sent_lens, name='sent_encoder') # (batch_size*max_art_len, max_sent_len, hidden_dim*2)
+      sent_lens = tf.reshape(self._sent_lens, [-1]) # (batch_size*max_art_len, )
+      sent_enc_outputs = self._add_encoder(sent_enc_inputs, sent_lens, name='sent_encoder') # (batch_size*max_art_len, max_sent_len, hidden_dim*2)
 
       # Add sentence-level encoder to produce sentence representations.
       # sentence-level encoder input: average-pooled, concatenated hidden states of the word-level bi-LSTM.
-      art_enc_inputs = tf.reduce_mean(sent_enc_outputs, axis=1) # (batch_size*max_art_len, hidden_dim*2)
+      sent_padding_mask = tf.reshape(self._sent_padding_mask, [-1, hps.max_sent_len, 1]) # (batch_size*max_art_len, max_sent_len, 1)
+      sent_lens_float = tf.reduce_sum(sent_padding_mask, axis=1)
+      self.sent_lens_float = tf.where(sent_lens_float > 0.0, sent_lens_float, tf.ones(sent_lens_float.get_shape().as_list()))
+      art_enc_inputs = tf.reduce_sum(sent_enc_outputs * sent_padding_mask, axis=1) / self.sent_lens_float # (batch_size*max_art_len, hidden_dim*2)
       art_enc_inputs = tf.reshape(art_enc_inputs, [hps.batch_size, -1, hps.hidden_dim*2]) # (batch_size, max_art_len, hidden_dim*2)
       art_enc_outputs = self._add_encoder(art_enc_inputs, self._art_lens, name='art_encoder') # (batch_size, max_art_len, hidden_dim*2)
 
       # Get each sentence representation and the document representation.
+      """
       sent_feats = tf.contrib.layers.fully_connected(art_enc_outputs, hps.hidden_dim*2, activation_fn=tf.tanh) # (batch_size, max_art_len, hidden_dim*2)
       art_feats = tf.reduce_mean(art_enc_outputs, axis=1) # (batch_size, hidden_dim*2)
       art_feats = tf.contrib.layers.fully_connected(art_feats, hps.hidden_dim*2, activation_fn=tf.tanh) # (batch_size, hidden_dim*2)
+      """
+      sent_feats = tf.contrib.layers.fully_connected(art_enc_outputs, hps.hidden_dim, activation_fn=tf.tanh) # (batch_size, max_art_len, hidden_dim)
+      art_padding_mask = tf.expand_dims(self._art_padding_mask, 2) # (batch_size, max_art_len, 1)
+      art_feats = tf.reduce_sum(art_enc_outputs * art_padding_mask, axis=1) / tf.reduce_sum(art_padding_mask, axis=1) # (batch_size, hidden_dim)
+      art_feats = tf.contrib.layers.fully_connected(art_feats, hps.hidden_dim, activation_fn=tf.tanh) # (batch_size, hidden_dim)
 
       ########################################
       # Add the classifier.                  #
       ########################################
-      logits = self._add_classifier(sent_feats, art_feats) # (batch_size, max_art_len)
+      logits, self.probs = self._add_classifier(sent_feats, art_feats) # (batch_size, max_art_len)
 
       ################################################
       # Calculate the loss                           #
       ################################################
       with tf.variable_scope('loss'):
-        self._loss = tf.sigmoid_cross_entropy_with_logits(logits=logits, labels=self._target_batch)
+        loss = tf.nn.sigmoid_cross_entropy_with_logits(logits=logits, labels=self._target_batch) # (batch_size, max_art_len)
+        loss = tf.reduce_sum(loss * self._art_padding_mask, 1) / tf.reduce_sum(self._art_padding_mask, 1) # (batch_size,)
+        self._loss = tf.reduce_mean(loss)
         tf.summary.scalar('loss', self._loss)
 
 
@@ -213,27 +245,28 @@ class SentenceSelector(object):
        summaries, loss, global_step and (optionally) coverage loss."""
     hps = self._hps
     feed_dict = self._make_feed_dict(batch)
-    pdb.set_trace()
 
     to_return = {
         'train_op': self._train_op,
         'summaries': self._summaries,
         'loss': self._loss,
+        'probs': self.probs,
         'global_step': self.global_step,
     }
     return sess.run(to_return, feed_dict)
 
-  def run_eval_step(self, sess, batch):
+  def run_eval_step(self, sess, batch, probs_only=False):
     """Runs one evaluation iteration. Returns a dictionary containing summaries, 
        loss, global_step and (optionally) coverage loss."""
     hps = self._hps
     feed_dict = self._make_feed_dict(batch)
 
-    to_return = {
-        'summaries': self._summaries,
-        'loss': self._loss,
-        'global_step': self.global_step,
-    }
-    return sess.run(to_return, feed_dict)
+    to_return = {'probs': self.probs}
 
+    if not probs_only:
+      to_return['summaries'] = self._summaries
+      to_return['loss'] = self._loss
+      to_return['global_step'] = self.global_step
+
+    return sess.run(to_return, feed_dict)
 

sentence-selector/run_selection.py
@@ -25,7 +25,7 @@ from collections import namedtuple
 from data import Vocab
 from batcher import Batcher
 from model import SentenceSelector
-from decode import BeamSearchDecoder
+from evaluate import SelectorEval
 import util
 import pdb
 
@@ -36,24 +36,35 @@ tf.app.flags.DEFINE_string('data_path', '', 'Path expression to tf.Example dataf
 tf.app.flags.DEFINE_string('vocab_path', '', 'Path expression to text vocabulary file.')
 
 # Important settings
-tf.app.flags.DEFINE_string('mode', 'train', 'must be one of train/eval/decode')
+tf.app.flags.DEFINE_string('mode', 'train', 'must be one of train/eval/eval_all')
+tf.app.flags.DEFINE_boolean('single_pass', False, '')
 
 # Where to save output
 tf.app.flags.DEFINE_integer('max_train_iter', 29000, 'max iterations to train')
-tf.app.flags.DEFINE_integer('save_model_every', 100, 'save the model every N iterations')
+tf.app.flags.DEFINE_integer('save_model_every', 10, 'save the model every N iterations')
 tf.app.flags.DEFINE_integer('model_max_to_keep', 3, 'save latest N models')
 tf.app.flags.DEFINE_string('log_root', '', 'Root directory for all logging.')
 tf.app.flags.DEFINE_string('exp_name', '', 'Name for experiment. Logs will be saved in a directory with this name, under log_root.')
+
+# evaluation settings
 tf.app.flags.DEFINE_string('decode_ckpt_path', '', 'checkpoint path for decoding')
+tf.app.flags.DEFINE_float('thres', 0.4, 'threshold of probabilities')
+tf.app.flags.DEFINE_integer('min_select_sent', 5, 'min sentences need to be selected')
+tf.app.flags.DEFINE_integer('max_select_sent', 20, 'max sentences to be selected')
+tf.app.flags.DEFINE_boolean('eval_rouge', False, 'whether to evaluate ROUGE scores')
+tf.app.flags.DEFINE_boolean('save_pkl', False, 'whether to save the results as pickle files')
+tf.app.flags.DEFINE_boolean('save_bin', True, 'whether to save the results as binary files')
+tf.app.flags.DEFINE_boolean('plot', True, 'whether to plot the precision/recall and recall/ratio curves')
 
 # Hyperparameters
+tf.app.flags.DEFINE_string('rnn_type', 'LSTM', 'LSTM/GRU')
 tf.app.flags.DEFINE_integer('hidden_dim', 200, 'dimension of RNN hidden states')
 tf.app.flags.DEFINE_integer('emb_dim', 128, 'dimension of word embeddings')
 tf.app.flags.DEFINE_integer('batch_size', 64, 'minibatch size')
-tf.app.flags.DEFINE_integer('max_art_len', 100, 'max timesteps of encoder (max source text tokens)')
-tf.app.flags.DEFINE_integer('max_sent_len', 50, 'max timesteps of decoder (max summary tokens)')
+tf.app.flags.DEFINE_integer('max_art_len', 100, 'max timesteps of word-level encoder')
+tf.app.flags.DEFINE_integer('max_sent_len', 50, 'max timesteps of sentence-level encoder')
 tf.app.flags.DEFINE_integer('vocab_size', 50000, 'Size of vocabulary. These will be read from the vocabulary file in order. If the vocabulary file contains fewer words than this number, or if this number is set to 0, will take all words in the vocabulary file.')
-tf.app.flags.DEFINE_float('lr', 0.15, 'learning rate')
+tf.app.flags.DEFINE_float('lr', 0.1, 'learning rate')
 tf.app.flags.DEFINE_float('adagrad_init_acc', 0.1, 'initial accumulator value for Adagrad')
 tf.app.flags.DEFINE_float('rand_unif_init_mag', 0.02, 'magnitude for lstm cells random uniform inititalization')
 tf.app.flags.DEFINE_float('trunc_norm_init_std', 1e-4, 'std of trunc norm init, used for initializing everything else')
@@ -85,7 +96,6 @@ def calc_running_avg_loss(loss, running_avg_loss, summary_writer, step, decay=0.
   summary_writer.add_summary(loss_sum, step)
   tf.logging.info('running_avg_loss: %f', running_avg_loss)
   return running_avg_loss
-
 
 def setup_training(model, batcher):
   """Does setup before starting training (run_training)"""
@@ -137,10 +147,22 @@ def run_training(model, batcher, sess_context_manager, sv, summary_writer):
       if not np.isfinite(loss):
         raise Exception("Loss is not finite. Stopping.")
 
-      # get the summaries and iteration number so we can write summaries to tensorboard
-      summaries = results['summaries'] # we will write these summaries to tensorboard using summary_writer
       train_step = results['global_step'] # we need this to update our running average loss
 
+      #calc_precision_recall(batch.original_articles_sents, batch.original_extract_sent_ids, results['probs'])
+      _, _, _, _, _, avg_p, avg_r, avg_a = util.get_batch_precision_recall(batch.original_articles_sents, batch.original_extract_sent_ids, results['probs'])
+      summary = tf.Summary()
+      summary.value.add(tag='SentSelector/avg_precision', simple_value=avg_p)
+      summary_writer.add_summary(summary, train_step)
+      summary = tf.Summary()
+      summary.value.add(tag='SentSelector/avg_recall', simple_value=avg_r)
+      summary_writer.add_summary(summary, train_step)
+      summary = tf.Summary()
+      summary.value.add(tag='SentSelector/avg_accuracy', simple_value=avg_a)
+      summary_writer.add_summary(summary, train_step)
+
+      # get the summaries and iteration number so we can write summaries to tensorboard
+      summaries = results['summaries'] # we will write these summaries to tensorboard using summary_writer
       summary_writer.add_summary(summaries, train_step) # write the summaries
       if train_step % 100 == 0: # flush the summary writer every so often
         summary_writer.flush()
@@ -177,7 +199,7 @@ def run_eval(model, batcher):
       if step == final_step:
         break
     
-    tf.logging.info('max_enc_steps: %d, max_dec_steps: %d', FLAGS.max_enc_steps, FLAGS.max_dec_steps)
+    #tf.logging.info('max_enc_steps: %d, max_dec_steps: %d', FLAGS.max_enc_steps, FLAGS.max_dec_steps)
     _ = util.load_ckpt(saver, sess) # load a new checkpoint
     batch = batcher.next_batch() # get the next batch
 
@@ -190,10 +212,22 @@ def run_eval(model, batcher):
     # print the loss and coverage loss to screen
     loss = results['loss']
     tf.logging.info('loss: %f', loss)
+    train_step = results['global_step']
+
+    #calc_precision_recall(batch.original_articles_sents, batch.original_extract_sent_ids, results['probs'])
+    _, _, _, _, _, avg_p, avg_r, avg_a = util.get_batch_precision_recall(batch.original_articles_sents, batch.original_extract_sent_ids, results['probs'])
+    summary = tf.Summary()
+    summary.value.add(tag='SentSelector/avg_precision', simple_value=avg_p)
+    summary_writer.add_summary(summary, train_step)
+    summary = tf.Summary()
+    summary.value.add(tag='SentSelector/avg_recall', simple_value=avg_r)
+    summary_writer.add_summary(summary, train_step)
+    summary = tf.Summary()
+    summary.value.add(tag='SentSelector/avg_accuracy', simple_value=avg_a)
+    summary_writer.add_summary(summary, train_step)
 
     # add summaries
     summaries = results['summaries']
-    train_step = results['global_step']
     summary_writer.add_summary(summaries, train_step)
 
     # calculate running avg loss
@@ -229,7 +263,7 @@ def main(unused_argv):
   vocab = Vocab(FLAGS.vocab_path, FLAGS.vocab_size) # create a vocabulary
 
   # Make a namedtuple hps, containing the values of the hyperparameters that the model needs
-  hparam_list = ['mode', 'lr', 'adagrad_init_acc', 'rand_unif_init_mag', 'trunc_norm_init_std', 'max_grad_norm', 'hidden_dim', 'emb_dim', 'batch_size', 'max_art_len', 'max_sent_len']
+  hparam_list = ['mode', 'rnn_type', 'lr', 'adagrad_init_acc', 'rand_unif_init_mag', 'trunc_norm_init_std', 'max_grad_norm', 'hidden_dim', 'emb_dim', 'batch_size', 'max_art_len', 'max_sent_len']
   hps_dict = {}
   for key,val in FLAGS.__flags.iteritems(): # for each flag
     if key in hparam_list: # if it's in the list
@@ -237,16 +271,22 @@ def main(unused_argv):
   hps = namedtuple("HParams", hps_dict.keys())(**hps_dict)
 
   # Create a batcher object that will create minibatches of data
-  batcher = Batcher(FLAGS.data_path, vocab, hps, single_pass=False)
+  if hps.mode == 'eval_all':
+    hps = hps._replace(batch_size=1)
+  batcher = Batcher(FLAGS.data_path, vocab, hps, single_pass=FLAGS.single_pass)
   tf.set_random_seed(111) # a seed value for randomness
 
   if hps.mode == 'train':
     print "creating model..."
-    model = SummarizationModel(hps, vocab)
+    model = SentenceSelector(hps, vocab)
     setup_training(model, batcher)
   elif hps.mode == 'eval':
-    model = SummarizationModel(hps, vocab)
+    model = SentenceSelector(hps, vocab)
     run_eval(model, batcher)
+  elif hps.mode == 'eval_all':
+    model = SentenceSelector(hps, vocab)
+    evaluator = SelectorEval(model, batcher, vocab)
+    evaluator.evaluate()
   else:
     raise ValueError("The 'mode' flag must be one of train/eval/decode")
 